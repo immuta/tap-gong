db05cf1b5a457ade7110ff663630fb0124f18372
Stephen Bailey <stkbailey@gmail.com>
2021-08-03 13:21:19 -0500

    Add properties to call_details

--- tap_gong/streams/call_details.py
+++ tap_gong/streams/call_details.py
@@ -17,45 +17,27 @@ class CallDetailsStream(GongStream):
     rest_method = "POST"
 
     schema = th.PropertiesList(
-        th.Property("id", th.StringType),
-        th.Property("metaData", th.ObjectType()),
+        th.Property("metaData", th.ObjectType(
+            th.Property("id", th.StringType),
+            th.Property("url", th.StringType),
+            th.Property("title", th.StringType),
+            th.Property("scheduled", th.StringType),
+            th.Property("started", th.StringType),
+            th.Property("duration", th.IntegerType),
+            th.Property("primaryUserId", th.StringType),
+            th.Property("direction", th.StringType),
+            th.Property("system", th.StringType),
+            th.Property("scope", th.StringType),
+            th.Property("media", th.StringType),
+            th.Property("language", th.StringType),
+            th.Property("workspaceId", th.StringType),
+            th.Property("sdrDisposition", th.StringType),
+            th.Property("clientUniqueId", th.StringType),
+            th.Property("customData", th.StringType),
+        )),
         th.Property("context", th.ArrayType(th.StringType)),
-        th.Property("parties", th.ArrayType(th.ObjectType(
-            th.Property("id", th.StringType),
-        ))),
-        th.Property("interaction", th.ObjectType(
-            th.Property("speakers", th.ObjectType()),
-        )),
-        th.Property("collaboration", th.ObjectType()),
-        th.Property("content", th.ObjectType(
-            th.Property("trackers", th.ArrayType(th.ObjectType())),
-            th.Property("topics", th.ArrayType(th.ObjectType())),
-        )),
-        th.ObjectType(
-            th.Property("id", th.StringType),
-            th.Property("metaData", th.ObjectType(
-                th.ObjectType(
-                th.Property("id", th.StringType),
-                th.Property("url", th.StringType),
-                th.Property("title", th.StringType),
-                th.Property("scheduled", th.StringType),
-                th.Property("started", th.StringType),
-                th.Property("duration", th.IntegerType),
-                th.Property("primaryUserId", th.StringType),
-                th.Property("direction", th.StringType),
-                th.Property("system", th.StringType),
-                th.Property("scope", th.StringType),
-                th.Property("media", th.StringType),
-                th.Property("language", th.StringType),
-                th.Property("workspaceId", th.StringType),
-                th.Property("sdrDisposition", th.StringType),
-                th.Property("clientUniqueId", th.StringType),
-                th.Property("customData", th.StringType),
-                ),
-            ),
-            th.Property("context", th.ArrayType(th.StringType)),
-            th.Property("parties", th.ArrayType(
-                th.ObjectType(
+        th.Property("parties", th.ArrayType(
+            th.ObjectType(
                 th.Property("id", th.StringType),
                 th.Property("emailAddress", th.StringType),
                 th.Property("name", th.StringType),
@@ -64,81 +46,70 @@ class CallDetailsStream(GongStream):
                 th.Property("speakerId", th.StringType),
                 th.Property("context", th.ArrayType(
                     th.ObjectType(
-                    th.Property("system", th.StringType),
-                    th.Property("objects", th.ArrayType(
-                        th.ObjectType(
-                        th.Property("objectType", th.StringType),
-                        th.Property("objectId", th.StringType),
-                        th.Property("fields", th.ArrayType(
+                        th.Property("system", th.StringType),
+                        th.Property("objects", th.ArrayType(
                             th.ObjectType(
-                            th.Property("name", th.StringType),
-                            th.Property("value", th.StringType),
-                            ),
+                                th.Property("objectType", th.StringType),
+                                th.Property("objectId", th.StringType),
+                                th.Property("fields", th.ArrayType(
+                                    th.ObjectType(
+                                        th.Property("name", th.StringType),
+                                        th.Property("value", th.StringType),
+                                    )),
+                                ),
+                            )),
                         ),
-                        ),
-                    ),
-                    ),
+                    )),
                 ),
                 th.Property("affiliation", th.StringType),
                 th.Property("methods", th.ArrayType(th.StringType)),
-                ),
-            ),
-            th.Property("content", th.ObjectType(
+            )),
+        ),
+        th.Property("content", th.ObjectType(
+            th.Property("trackers", th.ArrayType(
                 th.ObjectType(
-                th.Property("trackers", th.ArrayType(
-                    th.ObjectType(
                     th.Property("name", th.StringType),
                     th.Property("count", th.IntegerType),
-                    ),
-                ),
-                th.Property("topics", th.ArrayType(
-                    th.ObjectType(
+                )),
+            ),
+            th.Property("topics", th.ArrayType(
+                th.ObjectType(
                     th.Property("name", th.StringType),
                     th.Property("duration", th.IntegerType),
-                    ),
-                ),
-                th.Property("pointsOfInterest", th.ObjectType(
-                    th.ObjectType(
-                    th.Property("actionItems", th.ArrayType(th.StringType)),
-                    ),
-                ),
-                ),
+                )),
             ),
-            th.Property("interaction", th.ObjectType(
+            th.Property("pointsOfInterest", th.ObjectType(
+                th.Property("actionItems", th.ArrayType(th.StringType)),
+            )),
+        )),
+        th.Property("interaction", th.ObjectType(
+            th.Property("speakers", th.ArrayType(
                 th.ObjectType(
-                th.Property("speakers", th.ArrayType(
-                    th.ObjectType(
                     th.Property("id", th.StringType),
                     th.Property("userId", th.StringType),
                     th.Property("talkTime", th.FloatType),
-                    ),
-                ),
-                th.Property("interactionStats", th.ArrayType(
-                    th.ObjectType(
+                )),
+            ),
+            th.Property("interactionStats", th.ArrayType(
+                th.ObjectType(
                     th.Property("name", th.StringType),
                     th.Property("value", th.FloatType),
-                    ),
-                ),
-                th.Property("video", th.ArrayType(
-                    th.ObjectType(
+                )),
+            ),
+            th.Property("video", th.ArrayType(
+                th.ObjectType(
                     th.Property("name", th.StringType),
                     th.Property("duration", th.FloatType),
-                    ),
-                ),
-                th.Property("questions", th.ObjectType(
-                    th.ObjectType(
-                    th.Property("companyCount", th.IntegerType),
-                    th.Property("nonCompanyCount", th.IntegerType),
-                    ),
-                ),
-                ),
+                )),
             ),
-            th.Property("collaboration", th.ObjectType(
-                th.ObjectType(
-                ),
-            ),
-        ),
+            th.Property("questions", th.ObjectType(
+                th.Property("companyCount", th.IntegerType),
+                th.Property("nonCompanyCount", th.IntegerType),
+            )),
+        )),
+        th.Property("collaboration", th.ObjectType()),
     ).to_dict()
+
 
     def post_process(self, row: dict, context: Optional[dict]) -> dict:
         row["id"] = row["metaData"]["id"]
